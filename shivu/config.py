a89b002033ea6e807c310b4e68bc297eeb61b06e
arfin03 <156934678+arfin03@users.noreply.github.com>
2024-02-19 06:41:27 +0700

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -7,7 +7,7 @@ class Config(object):
     GROUP_ID = -1002015833205
     TOKEN = "6961758741:AAFjH5DEI1jiDJUG9WFEqv5hpdkDYvzaN8A"
     mongo_url = "mongodb+srv://arfin01:Arkaku123@cluster0.s2drx4p.mongodb.net/?retryWrites=true&w=majority"
-    PHOTO_URL = ["https://telegra.ph/file/43345aa900233f7649309.jpg", "https://telegra.ph/file/ee98b297887d361544b3c.jpg"]
+    PHOTO_URL = ["https://telegra.ph/file/1319554970a8697738eb3.jpg", "https://telegra.ph/file/33298b6daefbd0e3a20f4.jpg"]
     SUPPORT_CHAT = "gcanimecommunity"
     UPDATE_CHAT = "Origanimeinfo"
     BOT_USERNAME = "Guess_wwaifu_bot"
