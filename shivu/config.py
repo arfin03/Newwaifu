cd10c35908c684777400fe70b7906e1a24aa13a1
arfin03 <156934678+arfin03@users.noreply.github.com>
2024-03-18 15:44:25 +0700

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -7,7 +7,7 @@ class Config(object):
     GROUP_ID = -1002015833205
     TOKEN = "6872064337:AAGyofMcK81eno1XLCTG2f4LeRf9FacB-5Y"
     mongo_url = "mongodb+srv://arfin01:Arkaku123@cluster0.s2drx4p.mongodb.net/?retryWrites=true&w=majority"
-    PHOTO_URL = ["https://telegra.ph/file/1319554970a8697738eb3.jpg", "https://telegra.ph/file/33298b6daefbd0e3a20f4.jpg"]
+    PHOTO_URL = ["https://telegra.ph/file/3c876536b6e57ed8bf5bb.jpg", "https://telegra.ph/file/d4f4d52d126fc95fbe312.jpg"]
     SUPPORT_CHAT = "gcanimecommunity"
     UPDATE_CHAT = "guess_update"
     BOT_USERNAME = "Take_waifu_bot"
